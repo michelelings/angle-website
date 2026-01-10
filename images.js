1ab4705a317ea09fa11a45e3478bbb09195fa545
michelelings <71311796+michelelings@users.noreply.github.com>
2026-01-10 15:45:47 +0100

    Update imageData array in images.js: remove duplicate entries and add new images for enhanced gallery variety.

--- images.js
+++ images.js
@@ -1,23 +1,5 @@
 // Image data array
 const imageData = [
-    { src: 'images/1.webp', alt: 'Angle preview 1', title: 'Angle preview 1' },
-    { src: 'images/2.webp', alt: 'Angle preview 2', title: 'Angle preview 2' },
-    { src: 'images/3.webp', alt: 'Angle preview 3', title: 'Angle preview 3' },
-    { src: 'images/4.webp', alt: 'Angle preview 4', title: 'Angle preview 4' },
-    { src: 'images/5.webp', alt: 'Angle preview 5', title: 'Angle preview 5' },
-    { src: 'images/1.webp', alt: 'Angle preview 1', title: 'Angle preview 1' },
-    { src: 'images/2.webp', alt: 'Angle preview 2', title: 'Angle preview 2' },
-    { src: 'images/3.webp', alt: 'Angle preview 3', title: 'Angle preview 3' },
-    { src: 'images/4.webp', alt: 'Angle preview 4', title: 'Angle preview 4' },
-    { src: 'images/5.webp', alt: 'Angle preview 5', title: 'Angle preview 5' },
-    { src: 'images/1.webp', alt: 'Angle preview 1', title: 'Angle preview 1' },
-    { src: 'images/2.webp', alt: 'Angle preview 2', title: 'Angle preview 2' },
-    { src: 'images/3.webp', alt: 'Angle preview 3', title: 'Angle preview 3' },
-    { src: 'images/4.webp', alt: 'Angle preview 4', title: 'Angle preview 4' },
-    { src: 'images/5.webp', alt: 'Angle preview 5', title: 'Angle preview 5' },
-    { src: 'images/1.webp', alt: 'Angle preview 1', title: 'Angle preview 1' },
-    { src: 'images/2.webp', alt: 'Angle preview 2', title: 'Angle preview 2' },
-    { src: 'images/3.webp', alt: 'Angle preview 3', title: 'Angle preview 3' },
-    { src: 'images/4.webp', alt: 'Angle preview 4', title: 'Angle preview 4' },
-    { src: 'images/5.webp', alt: 'Angle preview 5', title: 'Angle preview 5' }
+    { src: 'images/6.webp', alt: 'Angle preview 6', title: 'Angle preview 6' },
+    { src: 'images/7.webp', alt: 'Angle preview 7', title: 'Angle preview 7' }
 ];
